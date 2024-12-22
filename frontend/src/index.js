dc3a5fd56e6cd3daf9186a3b0a5e942f951d4e21
AQRyu <daithanh.hua@gmail.com>
2024-12-22 18:39:36 +0700

    fix createRoot

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,12 @@
 import React from "react";
-
-//Utils
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-
-//Component
 import App from "./App";
 
-//Service Worker
-import * as serviceWorker from "./serviceWorker";
-
-ReactDOM.render(
-	<Router>
-		<App />
-	</Router>,
-	document.getElementById("root")
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <Router>
+    <App />
+  </Router>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
