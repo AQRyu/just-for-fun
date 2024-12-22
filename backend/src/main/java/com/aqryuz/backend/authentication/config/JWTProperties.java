10a1fc9c7600ada18a508697cd062e26fd2d8a79
AQRyu <daithanh.hua@gmail.com>
2024-12-22 19:42:11 +0700

    fix: expirationMs fix validation

--- backend/src/main/java/com/aqryuz/backend/authentication/config/JWTProperties.java
+++ backend/src/main/java/com/aqryuz/backend/authentication/config/JWTProperties.java
@@ -1,11 +1,13 @@
 package com.aqryuz.backend.authentication.config;
 
-import jakarta.validation.constraints.NotEmpty;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
 @Component
 @ConfigurationProperties(prefix = "jwt")
@@ -14,7 +16,8 @@ import org.springframework.validation.annotation.Validated;
 @Setter
 public class JWTProperties {
 
-  @NotEmpty private int expirationMs;
+  @NotNull
+  private Integer expirationMs;
 
   @NotEmpty private String secret;
 }
