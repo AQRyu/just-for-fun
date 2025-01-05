e6e81f00e5bb5e65de0134f1b849600624af2e41
AQRyu <daithanh.hua@gmail.com>
2025-01-06 00:40:40 +0700

    chore: auto-format code with Spotless before push

--- backend/src/main/java/com/aqryuz/backend/authentication/config/SecurityConfig.java
+++ backend/src/main/java/com/aqryuz/backend/authentication/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.aqryuz.backend.authentication.config;
 
 import java.util.List;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,8 +16,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
@@ -45,7 +43,7 @@ public class SecurityConfig {
         .cors(c -> c.configurationSource(corsConfigurationSource()))
         .sessionManagement(
             session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
   }
