d9c0f61a33b974a1deb321b495939bc3deb52c8e
AQRyu <daithanh.hua@gmail.com>
2025-01-05 23:04:00 +0700

    feat: Implement login functionality for UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Link, Route, Routes } from "react-router-dom";
 import Chat from "./components/Chat";
 import Profile from "./components/Profile";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -10,10 +11,12 @@ function App() {
       <nav>
         <Link to="/profile">Profile</Link>
         <Link to="/chat">Chat</Link>
+        <Link to="/login">Login</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="*" element={<p>Page not Found</p>} />{" "}
