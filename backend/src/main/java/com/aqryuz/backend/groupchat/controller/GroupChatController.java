3add99c4f7e2a01a2bdba5261882a3589b551a82
AQRyu <daithanh.hua@gmail.com>
2025-01-18 13:13:34 +0700

    chore: auto-format code with Spotless before push

--- backend/src/main/java/com/aqryuz/backend/groupchat/controller/GroupChatController.java
+++ backend/src/main/java/com/aqryuz/backend/groupchat/controller/GroupChatController.java
@@ -1,5 +1,12 @@
 package com.aqryuz.backend.groupchat.controller;
 
+import com.aqryuz.backend.authentication.model.User;
+import com.aqryuz.backend.groupchat.controller.payload.GroupChatCreationRequest;
+import com.aqryuz.backend.groupchat.model.Group;
+import com.aqryuz.backend.groupchat.service.GroupCreationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,15 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.aqryuz.backend.authentication.model.User;
-import com.aqryuz.backend.groupchat.controller.payload.GroupChatCreationRequest;
-import com.aqryuz.backend.groupchat.model.Group;
-import com.aqryuz.backend.groupchat.service.GroupCreationService;
-
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/groupchat")
