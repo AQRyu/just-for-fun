5e8e501b880ed9355534ca99a8f731366b91ee6c
AQRyu <daithanh.hua@gmail.com>
2025-01-19 22:23:29 +0700

    fix: fix workspace creation function handler admin wrongly

--- backend/src/main/java/com/aqryuz/backend/workspace/service/WorkspaceCreationServiceImpl.java
+++ backend/src/main/java/com/aqryuz/backend/workspace/service/WorkspaceCreationServiceImpl.java
@@ -1,5 +1,8 @@
 package com.aqryuz.backend.workspace.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.aqryuz.backend.authentication.model.User;
 import com.aqryuz.backend.authentication.repository.UserRepository;
 import com.aqryuz.backend.workspace.controller.payload.WorkspaceCreationRequest;
@@ -8,10 +11,8 @@ import com.aqryuz.backend.workspace.exception.WorkspaceMasterRequiredException;
 import com.aqryuz.backend.workspace.mapper.WorkspaceMapper;
 import com.aqryuz.backend.workspace.model.Workspace;
 import com.aqryuz.backend.workspace.repository.WorkspaceRepository;
-import java.util.Set;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -33,7 +34,7 @@ public class WorkspaceCreationServiceImpl implements WorkspaceCreationService {
 
     User user = userRepository.findById(userId).orElseThrow(WorkspaceMasterRequiredException::new);
 
-    Workspace newWorkspace = mapper.toWorkspace(request, Set.of(user));
+    Workspace newWorkspace = mapper.toWorkspace(request, user);
 
     return workspaceRepository.save(newWorkspace);
   }
